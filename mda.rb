3fd50d00f09e86df158842c8e7ee5476b3ebdafa
Babatunde Alli <tunsmart@gmail.com>
2015-08-23 10:35:18 +0100

    Initial commit, with the class defined and some logics

---
+++ mda.rb
@@ -0,0 +1,51 @@
+class MixedDentitionAnalysis
+
+	def initialize time
+		@name = name
+		@time = Time.now
+	end
+
+	#returns the amount of space required for the unerupted canines and premolars
+	#by taking in the mesio-distal width of the lower incissors using the Tanaka and Johnston formular
+	def tanaka_and_johnston_max md_width_li
+		md_width_uecap = 11 + 0.5 * md_width_li
+	end
+	
+	def tanaka_and_johnston_mand md_width_li
+		md_width_uecap = 10.5 + 0.5 * md_width_li 
+	end
+    #determines the space needed using the stanley and kerber prediction table
+	def stanley_and_kerber md_width_li_on_one_side
+		unless (23..30) === md_width_li_on_one_side 
+			raise ArgumentError,
+			"The sum of the mesio-distal width on one side of the mandible must be between 23-30mm "
+		end
+		prediction_table = { 23 => 18.4,24 => 19.0,25 => 19.7,26 => 20.3,27 => 21.0,28 => 21.6,29 => 22.3,30 => 22.9}
+		space_needed = prediction_table.fetch(md_width_li_on_one_side) #returns the space needed when supplied with the measured mesio-distal width of the incissors on one side of the mandible
+	end
+end
+
+puts "Hello, am Jake, welcome to the Mixed dentition analysis application, can i know your name?"
+
+name = gets.chomp #gets name from user
+
+puts "Hi Dr #{name}, now tell me which arch you intend to work on, type 'U' for upper jaw and 'L' for lower....U or L , which is it?"
+
+arch = gets.chomp.downcase! #gets whether upper or lower arch from the user
+  def help #defines a little logic to display to the user when they type in the incorrect arch option
+  	print "You must type in either U or L"
+  end
+case arch
+when "u"
+	puts "Since you are working on the upper arch Dr #{name}, please follow the following instructions carefully"
+	
+	space_analyser = MixedDentitionAnalysis.new(name) #creates a new object of the Mixed Dentition Analyser class
+
+	print "The Tanaka and Johnston method, will be used: 
+	   measure the mesio-distal width of each of the lower incissors, best done with the boley's guage, enter your values below....."
+when "l"
+	puts "honey"
+else
+	help
+	exit
+end
