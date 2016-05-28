c2eaf6e5022f0138c38ea3cec728473f1bde79eb
Babatunde Alli <tunsmart@gmail.com>
2016-05-28 15:54:38 +0100

    Modularised the messages too

--- mda.rb
+++ mda.rb
@@ -1,12 +1,9 @@
 require_relative "mixed_dentition_analyser"
+require_relative "messages"
 
-puts "Hello, am Jake, welcome to the Mixed dentition analysis application, can i know your name?"
+puts Message.greetings
 
-name = gets.chomp #gets name from user
-
-
-
-puts "Hi Dr #{name}, now tell me which arch you intend to work on, type 'U' for upper jaw and 'L' for lower....U or L , which is it?"
+puts Message.message1
 
 arch = gets.chomp #gets whether upper or lower arch from the user
 arch.downcase!
@@ -15,50 +12,40 @@ space_analyser = MixedDentitionAnalysis.new#creates a new object of the Mixed De
 
 case arch
 when "u"
-	puts "Since you are working on the upper arch Dr #{name},
-	      please follow the following instructions carefully"
-	puts "The Tanaka and Johnston method, will be used:
-	      measure the mesio-distal width of each of the lower incissors,
-		    best done with the boley's guage, enter your values, one after the other below....."
+	puts Message.message2
+	puts Message.message3
 		 values = []
 		 values << gets.to_i while values.length < 4
 		 md_width_li = values.inject(:+)
 		 space_required = space_analyser.space_analysed_upper(md_width_li)
-		 puts "The space required for the unerupted canine and premolar is #{space_required}mm.
-		       Thanks for using the mixed dentition analysis program"
+		 puts Message.message4 + " #{space_required}mm"
+		 puts Message.message5
 		 exit
 when "l"
-	puts "To analyze space needed in the Mandible, the Tanaka and Johnston
-	      or Stanley and Kerbers methods, can be used. type T for Tanaka, or S for Stanley"
+	puts Message.message6
 				method = gets.chomp
 				method.downcase!
 				case method
 				when "t"
-					puts "The Tanaka and Johnston method, will be used:
-					      measure the mesio-distal width of each of the lower incissors,
-						    best done with the boley's guage, enter your values, one after the other below....."
+					puts Message.message3
 								values = []
 							  values << gets.to_i while values.length < 4
 							  md_width_li = values.inject(:+)
 								space_required = space_analyser.space_analysed_lower(md_width_li)
-								puts "The space required for the unerupted canine and premolar is #{space_required}mm.
-							        Thanks for using the mixed dentition analysis program"
+								puts Message.message4 + " #{space_required}mm"
+								puts Message.message5
 							  exit
 
 				when "s"
-					puts "The Stanley and kerber Method will be used,
-					you should have a Periapical radiograph with you, please follow the following instructions carefully"
-					puts "Measure the mesio-distal width of the Permanent Mandibular Central and Lateral incissors"
-					puts "and input the SUM of their mesio-distal width below"
+					puts Message.message7
 					values = []
 					values << gets.to_i
-					puts "Finally measure the mesio-distal width of the erupting Canine and Premolar from the radiograph"
-					puts "and input the SUM of their mesio-distal width below too"
+					puts Message.message8
 					values << gets.to_i
 					final_values = values.inject(:+)
 					space_required = space_analyser.space_analysed_stanley_and_kerber(final_values)
-					puts "The space required for the unerupted canine and premolar is #{space_required}mm.
-								Thanks for using the mixed dentition analysis program"
+					puts Message.message4 + " #{space_required}mm"
+					puts Message.message5
 					exit
 
 				else
---
+++ messages.rb
@@ -0,0 +1,41 @@
+
+
+module Message
+
+def Message.greetings
+   %{Hello, welcome to the Mixed dentition analysis application, can i know your name?}
+ end
+def Message.message1
+  %{Now tell me which arch you intend to work on, type 'U' for upper jaw and 'L' for lower....U or L , which is it?}
+end
+def Message.message2
+  %{Since you are working on the upper arch,
+             please follow the following instructions carefully.}
+end
+def Message.message3
+  %{The Tanaka and Johnston method, will be used:
+    measure the mesio-distal width of each of the lower incissors,
+   best done with the boley's guage, enter your values, one after the other below.....}
+end
+def Message.message4
+  %{The space required for the unerupted canine and premolar is,}
+end
+def Message.message5
+  %{Thanks for using the mixed dentition analysis program}
+end
+def Message.message6
+  %{To analyze space needed in the Mandible, the Tanaka and Johnston
+      or Stanley and Kerbers methods, can be used. type T for Tanaka, or S for Stanley}
+end
+def Message.message7
+  %{The Stanley and kerber Method will be used,
+  you should have a Periapical radiograph with you, please follow the following instructions carefully,
+  Measure the mesio-distal width of the Permanent Mandibular Central and Lateral incissors,
+  and input the SUM of their mesio-distal width below.}
+end
+def Message.message8
+  %{Finally measure the mesio-distal width of the erupting Canine and Premolar from the radiograph,
+  and input the SUM of their mesio-distal width below too.}
+end
+
+end
